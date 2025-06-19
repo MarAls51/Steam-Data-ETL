92ef2a539b7bfae5e375102c34df4fbb5e188678
MA <wilcomeep123@gmail.com>
2025-06-19 14:39:16 -0400

    more data cleaning

--- ETL/ExtractData/ExtractSteamData.py
+++ ETL/ExtractData/ExtractSteamData.py
@@ -3,15 +3,23 @@ import requests
 import ExtractData.Pagination as Pagination
 import re
 
-def clean_data(data: list) -> pd.DataFrame:
+import pandas as pd
+import re
+import numpy as np
+
+def clean_data(data: list, row_na_threshold: float = 0.5, col_na_threshold: float = 0.8) -> pd.DataFrame:
     df = pd.DataFrame(data)
+
     df = df.map(lambda x: re.sub(r"[^A-Za-z0-9\s\-\.,!?]", "", str(x)) if pd.notna(x) else x)
-    df = df.apply(lambda col: col.map(lambda x: 0 if pd.isna(x) else x))
-    
+    df = df.applymap(lambda x: re.sub(r'\s+', ' ', x.strip()) if isinstance(x, str) else x)
+    df = df.apply(pd.to_numeric, errors='ignore')
+    df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
     df.replace("", pd.NA, inplace=True)
-    df.dropna(axis="rows", inplace=True)
+    df.dropna(axis=0, thresh=int(row_na_threshold * df.shape[1]), inplace=True)
+    df.dropna(axis=1, thresh=int((1 - col_na_threshold) * df.shape[0]), inplace=True)
+    df.fillna(0, inplace=True)
     df.drop_duplicates(inplace=True)
-    
+
     return df
 
 # Fetches all the steam games, works with my cron jobs. API returns the ID of the game as well as the name.
